547f005f1358fbe0c7dac075392ec3cfa016655d
f-ssemwanga <dssales@gmx.com>
2023-11-05 19:58:41 +0000

    added setter methods and time display method

--- task1_DigitalClock.py
+++ task1_DigitalClock.py
@@ -2,6 +2,17 @@ class Clock:
   '''Class definition for the digital clock class'''
   def __init__(self, currentHour, currentMinute) -> None:
     '''Constructor method'''
-    self.hours = currentHour
-    self.minutes = currentMinute
+    self.__hours = currentHour
+    self.__minutes = currentMinute
+    
+  def setHour(self, currentHour):
+    '''setter method for the private attribute hours '''
+    self.__hours = currentHour
+  def setMinute(self, currentMinute):
+    '''setter method for the private attribute minutes '''
+    self.__minutes = currentMinute
+  
+  def displayTime(self):
+    '''outputs the hours concatenated with the minutes'''
+    print(f'{self.__hours}:{self.__minutes}')
     
