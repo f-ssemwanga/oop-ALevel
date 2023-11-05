17200dc7aa446804716a53fca7d60fb84cd23a29
f-ssemwanga <dssales@gmx.com>
2023-11-05 19:49:48 +0000

    class definition and constructor

---
+++ task1_DigitalClock.py
@@ -0,0 +1,7 @@
+class Clock:
+  '''Class definition for the digital clock class'''
+  def __init__(self, currentHour, currentMinute) -> None:
+    '''Constructor method'''
+    self.hours = currentHour
+    self.minutes = currentMinute
+    
