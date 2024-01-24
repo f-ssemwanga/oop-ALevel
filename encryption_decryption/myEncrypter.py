56c7ead8bd23825876a20272e404ef7b55529400
Francis Ssemwanga <dssales@gmx.com>
2024-01-24 15:24:54 +0000

    Completed encryption and decryption method

--- encryption_decryption/myEncrypter.py
+++ encryption_decryption/myEncrypter.py
@@ -11,17 +11,27 @@ class Encypter:
     self.alphabet = list((ascii_letters+punctuation))
     self.key = 84690572193695274820765054327894631502
 
-  def encrypt(self, phrase):
+  def encrypt_decrypt(self, phrase,method):
     '''Encrypts and returns an encrypted phrase'''
     for  eachLetter in phrase:
       if eachLetter in self.alphabet:
         index = self.alphabet.index(eachLetter)
-        crypting = (index + self.key) % len(self.alphabet)
-        new = self.alphabet[crypting]
-        self.out.append(new)
+        if method =="encrypt":
+          crypting = (index + self.key) % len(self.alphabet)
+          new = self.alphabet[crypting]
+          self.out.append(new)
+          continue
+        elif method =="decrypt":
+          decrypting = (index - self.key) % len(self.alphabet)
+          new = self.alphabet[decrypting]
+          self.out.append(new)
       else:
         self.out.append(eachLetter)
-    return "".join(self.out)
+    return ("".join(self.out))
+    
+  
 
 testInst = Encypter()
-print(testInst.encrypt("You Muppet"))
+#print(testInst.encrypt_decrypt("You Muppet","encrypt"))
+#print()
+print(testInst.encrypt_decrypt("%uA SAvvkz", "decrypt"))
